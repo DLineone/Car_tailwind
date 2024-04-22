7fdedde6e804a25495deb4c5b118162e74a72a2d
DLineone <DLineone@users.noreply.github.com>
2024-04-22 03:15:11 +0300

    loading data

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
 import { Suspense } from "react";
 import CarCard from "./components/CarCard";
 import LoadingCarCard from "./components/LoadingCarCard";
+import useSWR from "swr";
+import { fetchCar } from "./utils";
 
 function App() {
-  let [car, setCar] = useState({
-    title: "BMW X5 30d, 2019",
-    img: "car.jpg",
-    description: ` Заинтересовал этот автомобиль? Позвоните продавцу, чтобы узнать детали — а мы пришлём вам Отчёт в подарок!
-    
-    ‼️Цена указана за наличный расчет ‼️
-    
-    Болee подрoбную информaцию по комплектaции aвтомoбиля и дpугие интepеcующие вoпросы утoчняйте пo телeфону, укaзaнному в объявлении.
-    
-    Автомобили с пробегом :
-    Возможность покупки автомобиля в кредит, более 10 банков.`,
-  });
   return (
     <div className="flex h-full w-full items-center justify-center bg-slate-400">
       <Suspense fallback={<LoadingCarCard />}>
-        <CarCard car={car} />
+        <CarCard />
       </Suspense>
     </div>
   );
--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -1,59 +1,73 @@
 import { useState } from "react";
+import useSWR from "swr";
+import { fetchCar } from "../utils";
 
-/* eslint-disable react/prop-types */
-function CarCard({ car }) {
+function CarCard() {
   const [modalOpen, setModalOpen] = useState(false);
+
+  const fetcher = async () =>
+    fetchCar().then((res) => {
+      console.log(res);
+      return res;
+    });
+
+  const { data: car } = useSWR("car", fetcher, {
+    suspense: true,
+  });
+  console.log(car);
 
   function switchModal() {
     setModalOpen((prev) => !prev);
   }
 
-  return (
-    <div className="relative">
-      <div className="peer relative z-10 flex max-w-sm content-center items-center gap-2 rounded-md bg-white p-2 shadow-lg transition  duration-200 sm:max-w-xl md:max-w-2xl">
-        <img
-          className="aspect-square w-52 object-contain"
-          src={car.img}
-          alt=""
-        />
-        <div className=" flex h-full flex-col content-start items-start gap-2">
-          <div className="font-bold">{car.title}</div>
-          <div className="word line-clamp-6 whitespace-pre-line break-words text-justify">
-            {car.description}
-          </div>
-          <button
-            className="self-end rounded bg-yellow-300 p-2 shadow drop-shadow-sm transition
+  if (car)
+    return (
+      <div className="relative">
+        <div className="peer relative z-10 flex max-w-sm content-center items-center gap-2 rounded-md bg-white p-2 shadow-lg transition  duration-200 sm:max-w-xl md:max-w-2xl">
+          <img
+            className="aspect-square w-52 object-contain"
+            src={car.img}
+            alt=""
+          />
+          <div className=" flex h-full flex-col content-start items-start gap-2">
+            <div className="font-bold">{car.title}</div>
+            <div className="word line-clamp-6 whitespace-pre-line break-words text-justify">
+              {car.description}
+            </div>
+            <button
+              className="self-end rounded bg-yellow-300 p-2 shadow drop-shadow-sm transition
           hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]
           active:translate-x-0 active:translate-y-0 active:shadow"
-            onClick={switchModal}
-          >
-            Подробнее
-          </button>
-        </div>
-      </div>
-      <span className="absolute left-0 top-0 -z-0 h-full w-full rounded-md bg-black transition peer-hover:translate-x-1 peer-hover:translate-y-1"></span>
-      <dialog
-        id="modal"
-        open={modalOpen}
-        className="absolute inset-0 z-20 max-w-sm  rounded-md p-2 sm:max-w-xl md:max-w-2xl"
-      >
-        <div className="flex flex-col items-start justify-start gap-2">
-          <div className="font-bold">{car.title}</div>
-          <div className="word whitespace-pre-line break-words text-justify">
-            {car.description}
+              onClick={switchModal}
+            >
+              Подробнее
+            </button>
           </div>
-          <button
-            className="self-end rounded bg-yellow-300 p-2 shadow drop-shadow-sm transition
+        </div>
+        <span className="absolute left-0 top-0 -z-0 h-full w-full rounded-md bg-black transition peer-hover:translate-x-1 peer-hover:translate-y-1"></span>
+        <dialog
+          id="modal"
+          open={modalOpen}
+          className="absolute inset-0 z-20 max-w-sm  rounded-md p-2 sm:max-w-xl md:max-w-2xl"
+        >
+          <div className="flex flex-col items-start justify-start gap-2">
+            <div className="font-bold">{car.title}</div>
+            <div className="word whitespace-pre-line break-words text-justify">
+              {car.description}
+            </div>
+            <button
+              className="self-end rounded bg-yellow-300 p-2 shadow drop-shadow-sm transition
             hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]
             active:translate-x-0 active:translate-y-0 active:shadow"
-            onClick={switchModal}
-          >
-            Закрыть
-          </button>
-        </div>
-      </dialog>
-    </div>
-  );
+              onClick={switchModal}
+            >
+              Закрыть
+            </button>
+          </div>
+        </dialog>
+      </div>
+    );
+  else return null;
 }
 
 export default CarCard;
